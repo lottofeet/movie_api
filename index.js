f4541df3fae30576399070480ba80c4ebcd37300
lottofeet <83425341+lottofeet@users.noreply.github.com>
2021-08-01 12:51:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 const express = require('express');
 const app = express();
+
+let Movies = [
+    {
+        title: 'Something about Mary',
+        director: 'someone'
+    },
+    {
+        title: 'movie 2',
+        director: 'someone'
+    },
+    {
+        title: 'movie 3',
+        director: 'someone else'
+    }
+];
+
+
+// GET requests
+app.get('/movies', (req, res) => {
+    res.json(Movies)
+});
+
+app.get('/', (req, res) => {
+    res.send('You have arrived to my Movie page!');
+});
+
+
+
+app.listen(8081, () => {
+    console.log('Your app is listening on port 8081.');
+});
